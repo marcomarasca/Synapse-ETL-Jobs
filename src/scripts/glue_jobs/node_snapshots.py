cf75459d9c7c4fe3ca79eef5956108fbe20ab578
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2023-04-04 22:21:01 -0700

    PLFM-7719: Fix for hidden glue error

--- src/scripts/glue_jobs/node_snapshots.py
+++ src/scripts/glue_jobs/node_snapshots.py
@@ -1,6 +1,6 @@
 """
-This script executed by a Glue job. The job take the access record data from S3 and process it.
- Processed data stored in S3 in a parquet file partitioned by timestamp of record as  year / month / day.
+The job take the node snapshot data from S3 and process it.
+Processed data stored in S3 in a parquet file partitioned by the date (%Y-%m-%d pattern) of the change timestamp.
 """
 
 import sys
@@ -11,70 +11,28 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-def get_dynamic_frame(connection_type, file_format, source_path, glue_context):
-    dynamic_frame = glue_context.create_dynamic_frame.from_options(
-        format_options={"multiline": True},
-        connection_type=connection_type,
-        format=file_format,
-        connection_options={
-            "paths": [source_path],
-            "recurse": True,
-        },
-        transformation_ctx="dynamic_frame")
-    return dynamic_frame
-
-def apply_mapping(dynamic_frame):
-    
-    mapped_dynamic_frame = ApplyMapping.apply(
-        frame=dynamic_frame,
-        mappings=[
-            ("changeType",                      "string",   "change_type",          "string"),
-            ("changeTimestamp",                 "bigint",   "change_timestamp",     "bigint"),
-            ("changeUserId",                    "bigint",   "change_user_id",       "bigint"),
-            ("snapshotTimestamp",               "bigint",   "snapshot_timestamp",   "bigint"),
-            ("snapshot.id",                     "string",   "id",                   "bigint"),
-            ("snapshot.benefactorId",           "string",   "benefactor_id",        "bigint"),
-            ("snapshot.projectId",              "string",   "project_id",           "bigint"),
-            ("snapshot.parentId",               "string",   "parent_id",            "bigint"),
-            ("snapshot.nodeType",               "string",   "node_type",            "string"),
-            ("snapshot.createdOn",              "bigint",   "created_on",           "bigint"),
-            ("snapshot.createdByPrincipalId",   "bigint",   "created_by",           "bigint"),
-            ("snapshot.modifiedOn",             "bigint",   "modified_on",          "bigint"),
-            ("snapshot.modifiedByPrincipalId",  "bigint",   "modified_by",          "bigint"),
-            ("snapshot.versionNumber",          "bigint",   "version_number",       "bigint"),
-            ("snapshot.fileHandleId",           "string",   "file_handle_id",       "bigint"),
-            ("snapshot.name",                   "string",   "name",                 "string"),
-            ("snapshot.isPublic",               "boolean",  "is_public",            "boolean"),
-            ("snapshot.isControlled",           "boolean",  "is_controlled",        "boolean"),
-            ("snapshot.isRestricted",           "boolean",  "is_restricted",        "boolean"),
-            ("change_date",                     "string",   "change_date",          "string"),
-        ],
-        transformation_ctx="mapped_dynamic_frame"
-    )
-
-    return mapped_dynamic_frame
-
 def strip_syn_prefix(input_string):
     if input_string is None:
         return input_string
-
+    
     if input_string.startswith('syn'):
         return input_string[len('syn'):]
 
     return input_string
 
+# process the access record
 def transform(dynamic_record):
-    date = datetime.utcfromtimestamp(dynamic_record["changeTimestamp"] / 1000.0)
+    date = datetime.utcfromtimestamp(dynamic_record["change_timestamp"] / 1000.0)
     
-    # Add the partition date from the timestamp of the change
+    # This is the partition date
     dynamic_record["change_date"] = date.strftime("%Y-%m-%d")
     
     # The records come in with the syn prefix, we need to remove that
-    dynamic_record["snapshot"]["id"] = strip_syn_prefix(dynamic_record["snapshot"]["id"])
-    dynamic_record["snapshot"]["benefactorId"] = strip_syn_prefix(dynamic_record["snapshot"]["benefactorId"])
-    dynamic_record["snapshot"]["projectId"] = strip_syn_prefix(dynamic_record["snapshot"]["projectId"])
-    dynamic_record["snapshot"]["parentId"] = strip_syn_prefix(dynamic_record["snapshot"]["parentId"])
-    dynamic_record["snapshot"]["fileHandleId"] = strip_syn_prefix(dynamic_record["snapshot"]["fileHandleId"])
+    dynamic_record["id"] = strip_syn_prefix(dynamic_record["id"])
+    dynamic_record["benefactor_id"] = strip_syn_prefix(dynamic_record["benefactor_id"])
+    dynamic_record["project_id"] = strip_syn_prefix(dynamic_record["project_id"])
+    dynamic_record["parent_id"] = strip_syn_prefix(dynamic_record["parent_id"])
+    dynamic_record["file_handle_id"] = strip_syn_prefix(dynamic_record["file_handle_id"])
 
     return dynamic_record
 
@@ -86,19 +44,64 @@ def main():
     job = Job(glue_context)
     job.init(args["JOB_NAME"], args)
 
-    dynamic_frame = get_dynamic_frame("s3", "json", args["S3_SOURCE_PATH"], glue_context)
-    transformed_dynamic_frame = dynamic_frame.map(f=transform)
-    mapped_dynamic_frame = apply_mapping(transformed_dynamic_frame)
+    input_frame = glue_context.create_dynamic_frame.from_options(
+        format_options={"multiline": True},
+        connection_type="s3",
+        format="json",
+        connection_options={
+            "paths": [args["S3_SOURCE_PATH"]],
+            "recurse": True
+        }
+    )
+
+    # Maps the incoming record to a flatten table
+    mapped_frame = input_frame.apply_mapping(
+        [
+            ("changeType",                      "string",   "change_type",          "string"),
+            ("changeTimestamp",                 "bigint",   "change_timestamp",     "bigint"),
+            ("userId",                          "bigint",   "change_user_id",       "bigint"),
+            ("snapshotTimestamp",               "bigint",   "snapshot_timestamp",   "bigint"),
+            ("snapshot.id",                     "string",   "id",                   "string"),
+            ("snapshot.benefactorId",           "string",   "benefactor_id",        "string"),
+            ("snapshot.projectId",              "string",   "project_id",           "string"),
+            ("snapshot.parentId",               "string",   "parent_id",            "string"),
+            ("snapshot.nodeType",               "string",   "node_type",            "string"),
+            ("snapshot.createdOn",              "bigint",   "created_on",           "bigint"),
+            ("snapshot.createdByPrincipalId",   "bigint",   "created_by",           "bigint"),
+            ("snapshot.modifiedOn",             "bigint",   "modified_on",          "bigint"),
+            ("snapshot.modifiedByPrincipalId",  "bigint",   "modified_by",          "bigint"),
+            ("snapshot.versionNumber",          "bigint",   "version_number",       "bigint"),
+            ("snapshot.fileHandleId",           "string",   "file_handle_id",       "string"),
+            ("snapshot.name",                   "string",   "name",                 "string"),
+            ("snapshot.isPublic",               "boolean",  "is_public",            "boolean"),
+            ("snapshot.isControlled",           "boolean",  "is_controlled",        "boolean"),
+            ("snapshot.isRestricted",           "boolean",  "is_restricted",        "boolean"),
+        ]
+    )
+
+    # Apply transformations (compute the partition and get rid of syn prefix)
+    transformed_frame = mapped_frame.map(f=transform)
+
+    # Now cast the "ids" to actual long
+    output_frame = transformed_frame.resolveChoice(
+        [
+            ("id", "cast:bigint"),
+            ("benefactor_id", "cast:bigint"),
+            ("project_id", "cast:bigint"),
+            ("parent_id", "cast:bigint"),
+            ("file_handle_id", "cast:bigint")
+        ]
+    )
 
     glue_context.write_dynamic_frame.from_catalog(
-        frame=mapped_dynamic_frame,
+        frame=output_frame,
         database=args["DATABASE_NAME"],
         table_name=args["TABLE_NAME"],
-        additional_options={"partitionKeys": ["change_date"]},
-        transformation_ctx="write_dynamic_frame"
+        additional_options={"partitionKeys": ["change_date"]}
     )
 
     job.commit()
+
 
 if __name__ == "__main__":
     main()
