8186bacd1cef701aac3d816d6cdabccf929cfdb5
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2023-04-13 11:23:41 -0700

    PLFM-7783: Fix for syn id failure on None input

--- src/scripts/glue_jobs/utils.py
+++ src/scripts/glue_jobs/utils.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 
-def strip_syn_prefix(input_string):
+def syn_id_string_to_int(input_string):
     if input_string is None:
         return input_string
-    
+
+    input_string = input_string.strip()
+
     if input_string.startswith('syn'):
-        return input_string[len('syn'):]
+        input_string = input_string[len('syn'):]
+
+    if not input_string:
+        return None
         
-    return input_string
+    return int(input_string)
 
 def ms_to_partition_date(timestamp_ms):
     return datetime.utcfromtimestamp(timestamp_ms / 1000.0).strftime("%Y-%m-%d")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,21 +4,29 @@ from src.scripts.glue_jobs import utils
 
 class TestUtils(unittest.TestCase):
 
-    def test_strip_syn_prefix(self):
+    def test_syn_id_string_to_int(self):
         input = "syn123"
-        self.assertEqual("123", utils.strip_syn_prefix(input))
+        self.assertEqual(123, utils.syn_id_string_to_int(input))
 
-    def test_strip_syn_prefix_with_no_syn_prefix(self):
+    def test_syn_id_string_to_int_with_no_syn_prefix(self):
         input = "123"
-        self.assertEqual("123", utils.strip_syn_prefix(input))
-
-    def test_strip_syn_prefix_with_empty_string(self):
-        input = ""
-        self.assertEqual("", utils.strip_syn_prefix(input))
+        self.assertEqual(123, utils.syn_id_string_to_int(input))
     
-    def test_strip_syn_prefix_with_none_input(self):
+    def test_syn_id_string_to_int_with_long_value(self):
+        input = "9223372036854775807"
+        self.assertEqual(9223372036854775807, utils.syn_id_string_to_int(input))
+
+    def test_syn_id_string_to_int_with_empty_string(self):
+        input = ""
+        self.assertEqual(None, utils.syn_id_string_to_int(input))
+
+    def test_syn_id_string_to_int_with_blank_string(self):
+        input = "  "
+        self.assertEqual(None, utils.syn_id_string_to_int(input))
+    
+    def test_syn_id_string_to_int_with_none_input(self):
         input = None
-        self.assertEqual(None, utils.strip_syn_prefix(input))
+        self.assertEqual(None, utils.syn_id_string_to_int(input))
 
     def test_ms_to_partition_date(self):
         input = 1680758367496
