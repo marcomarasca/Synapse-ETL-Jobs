40b5edd33b5b98aea30d2e7e52d102bc991b15f2
SandhraSokhal <sandhra.sokhal@sagebase.org>
2023-08-17 15:11:33 -0700

    file download backfill script

--- src/scripts/backfill_jobs/backfill_utils.py
+++ src/scripts/backfill_jobs/backfill_utils.py
@@ -6,3 +6,13 @@ def get_date_from_absolute_file_name(file_name):
     second_last_index = file_name.rfind("/", 0, last_slash_index - 1)
     date_string = file_name[second_last_index + 1: last_slash_index]
     return datetime.strptime(date_string, '%Y-%m-%d')
+
+
+def ms_to_partition_date(timestamp_ms):
+    return datetime.utcfromtimestamp(timestamp_ms / 1000.0)
+
+
+def remove_padded_leading_zeros(input_string):
+    if input_string is None:
+        return input_string
+    return input_string.lstrip("0")
