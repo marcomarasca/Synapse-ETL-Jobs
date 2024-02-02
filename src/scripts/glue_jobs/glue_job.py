bbad31c356f9e58e082d1b5d9b3f173d42b47b30
SandhraSokhal <sandhra.sokhal@sagebase.org>
2024-02-01 21:13:29 -0800

    code refactoring

--- src/scripts/glue_jobs/glue_job.py
+++ src/scripts/glue_jobs/glue_job.py
@@ -56,6 +56,7 @@ class GlueJob:
             # Note: even though this is optional, job bookmark does not work without it
             transformation_ctx="input_frame"
         )
+        self.check_and_log_errors(input_frame)
         self.logger.info("The total number of records retrieved from s3 is {}".format(input_frame.count()))
         return input_frame
 
@@ -63,8 +64,7 @@ class GlueJob:
     def apply_mappings(self, mapping_list, dynamic_frame):
         if mapping_list is not None and len(mapping_list) > 0:
             mapped_frame = dynamic_frame.apply_mapping(mapping_list)
-            if mapped_frame.stageErrorsCount() > 0:
-                self.log_errors(mapped_frame)
+            self.check_and_log_errors(mapped_frame)
             return mapped_frame
         return dynamic_frame
 
@@ -74,13 +74,11 @@ class GlueJob:
 
     # Resolve the data type mismatch by comparing it with table schema and store the processed data back to s3
     def resolve_choice_and_write_output_frame(self, transformed_frame, partition_key):
-        if transformed_frame.stageErrorsCount() > 0:
-            self.log_errors(transformed_frame)
+        self.check_and_log_errors(transformed_frame)
 
         output_frame = transformed_frame.resolveChoice(choice='match_catalog', database=self.args['DATABASE_NAME'],
                                                        table_name=self.args['TABLE_NAME'])
-        if output_frame.stageErrorsCount() > 0:
-            self.log_errors(output_frame)
+        self.check_and_log_errors(output_frame)
 
         if output_frame.count() > 0:
             self.logger.info("The total number of records saved to s3 is {}".format(output_frame.count()))
@@ -93,15 +91,15 @@ class GlueJob:
         self.job.commit()
         self.logger.info("Glue job finished.")
 
-    def log_errors(self, dynamic_frame):
-        self.logger.error(
-            "The total number of error count in dynamic frame is {} ".format(dynamic_frame.stageErrorsCount()))
-        df = dynamic_frame.errorsAsDynamicFrame().toDF()
-        last_error_msg = "UNKNOWN"
-        for row in df.rdd.collect():
-            error_fields = row["error"]
-            for key in error_fields.asDict().keys():
-                self.logger.error("{} : {}".format(key, error_fields[key]))
-            if error_fields["msg"] is not None:
-                last_error_msg = error_fields["msg"]
-        raise Exception("Job failed with error : {}".format(last_error_msg))
+    def check_and_log_errors(self, dynamic_frame):
+        if dynamic_frame.stageErrorsCount() > 0:
+            self.logger.error(
+                "The total number of error count in dynamic frame is {} ".format(dynamic_frame.stageErrorsCount()))
+            df = dynamic_frame.errorsAsDynamicFrame().toDF()
+            last_error_msg = "UNKNOWN"
+            for row in df.rdd.collect():
+                error_fields = row["error"]
+                self.logger.error("Error in record : {}".format(row))
+                if error_fields["msg"] is not None:
+                    last_error_msg = error_fields["msg"]
+            raise Exception("Job failed with error : {}".format(last_error_msg))
